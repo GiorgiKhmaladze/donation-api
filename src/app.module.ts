41bc265fe8d1e0fcc085658eee4fecf41acb94f6
Giorgi Khmaladze <giorgi.khmaladze@edetek.comm>
2020-08-27 13:47:23 +0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
 import { ProductsModule } from './products/products.module';
 import { WidgetsModule } from './widgets/widgets.module';
 
 @Module({
-  imports: [ProductsModule, WidgetsModule, MongooseModule.forRoot('mongodb://localhost/donation', {
+  imports: [ProductsModule, WidgetsModule, EventsModule, MongooseModule.forRoot('mongodb://localhost/donation', {
     useNewUrlParser: true
   })],
 
